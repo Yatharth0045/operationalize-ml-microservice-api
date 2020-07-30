4a636beede41d0c6d163e11925da851e5c29a378
Yatharth Sharma <yatharth0045@gmail.com>
2020-07-30 13:08:43 +0530

    added application and docker related files

---
+++ run_docker.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+TAG="ml-app:1.0.0"
+# Step 1:
+# Build image and add a descriptive tag
+docker build -t ${TAG} .
+
+# Step 2: 
+# List docker images
+docker images | grep ${TAG}
+
+# Step 3: 
+# Run flask app
+docker run ${TAG}
